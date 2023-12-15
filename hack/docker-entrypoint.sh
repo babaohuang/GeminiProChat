109cfc16df3e8d1600b1351571347d2f3c49c5f7
babaohuang <babaohuang@gmail.com>
2023-12-15 20:56:39 +0800

    Update docker-entrypoint.sh

--- hack/docker-entrypoint.sh
+++ hack/docker-entrypoint.sh
@@ -4,27 +4,27 @@ sub_service_pid=""
 
 sub_service_command="node dist/server/entry.mjs"
 
-function init() {
+init() {
     /bin/sh ./docker-env-replace.sh
 }
 
-function main {
+main() {
   init
 
   echo "Starting service..."
   eval "$sub_service_command &"
   sub_service_pid=$!
 
-  trap cleanup SIGTERM SIGINT
+  trap 'cleanup' SIGTERM SIGINT
   echo "Running script..."
-  while [ true ]; do
+  while true; do
       sleep 5
   done
 }
 
-function cleanup {
+cleanup() {
   echo "Cleaning up!"
-  kill -TERM $sub_service_pid
+  kill -TERM "$sub_service_pid"
 }
 
 main
