e205400894a014fea16f6f2a6279c68d4a578fef
babaohuang <babaohuang@gmail.com>
2023-12-17 12:21:07 +0800

    Update openAI.ts

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -1,7 +1,12 @@
 import { GoogleGenerativeAI } from '@fuyun/generative-ai'
 
 const apiKey = process.env.GEMINI_API_KEY
-const genAI = new GoogleGenerativeAI(apiKey)
+const apiBaseUrl = process.env.API_BASE_URL
+
+// 使用条件运算符来决定是否传递 baseURL
+const genAI = apiBaseUrl
+  ? new GoogleGenerativeAI(apiKey, apiBaseUrl)
+  : new GoogleGenerativeAI(apiKey)
 
 export const startChatAndSendMessageStream = async(history: ChatMessage[], newMessage: string) => {
   const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
