288f7033c050ac3c81b950b7637762d5f03bca7c
babaohuang <babaohuang@gmail.com>
2023-12-17 20:10:04 +0800

    Revert "Update openAI.ts"
    This reverts commit a51abd2c644fb761996a0b1375f9d3eb58d59f50.

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -7,27 +7,8 @@ const genAI = apiBaseUrl
   ? new GoogleGenerativeAI(apiKey, apiBaseUrl)
   : new GoogleGenerativeAI(apiKey)
 
-const safetySettings = [
-  {
-    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
-]
-
 export const startChatAndSendMessageStream = async(history: ChatMessage[], newMessage: string) => {
-  const model = genAI.getGenerativeModel({ model: 'gemini-pro', safetySettings })
+  const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
 
   const chat = model.startChat({
     history: history.map(msg => ({
