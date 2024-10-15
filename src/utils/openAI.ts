1ceaa3cb8012586bc766b7e4c438a7fd7a3db4fe
Adonis <75648240+Bucky-26@users.noreply.github.com>
2024-10-15 21:26:37 +0800

    openAI.ts

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI } from '@fuyun/generative-ai'
-import { defaultChatMessage } from '../types' // Import the default chat message
+import { ChatMessage } from '../types' // Import ChatMessage only
 
 const apiKey = (import.meta.env.GEMINI_API_KEY)
 const apiBaseUrl = (import.meta.env.API_BASE_URL)?.trim().replace(/\/$/, '')
@@ -8,16 +8,14 @@ const genAI = apiBaseUrl
   ? new GoogleGenerativeAI(apiKey, apiBaseUrl)
   : new GoogleGenerativeAI(apiKey)
 
-export const startChatAndSendMessageStream = async(history: ChatMessage[], newMessage: string) => {
+export const startChatAndSendMessageStream = async (history: ChatMessage[], newMessage: string) => {
   const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
 
-  // Check if the chat history is empty, indicating this is the user's first interaction
-  if (history.length === 0) {
-    history.push({
-      role: 'model',
-      parts: [{ text: defaultChatMessage }] // Use default chat message if history is empty
-    });
-  }
+  // Always insert the default message into the history
+  history.push({
+    role: 'model',
+    parts: [{ text: "I'm EASY AI developed by ADONIS JR S under EASY API headed by Adonis himself." }] // Default message
+  });
 
   const chat = model.startChat({
     history: history.map(msg => ({
